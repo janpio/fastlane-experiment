b98cac2764cf9fd6f410c795eefb50d1605e85fc
Jan Piotrowski <piotrowski+git@gmail.com>
2018-12-18 17:09:11 +0100

    clean up shell script

--- add-fastlane-repo-and-branch-to-Gemfile.sh
+++ add-fastlane-repo-and-branch-to-Gemfile.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-
-pwd
-echo "$REPO"
-echo "$BRANCH"
 if [ -n "$REPO" ] && [ -n "$BRANCH" ]; then
     SEARCH='gem "fastlane".*'
-    echo "$SEARCH"
     REPLACE="gem \"fastlane\", :git => \"$REPO\", :branch => \"$BRANCH\""
-    echo "$REPLACE"
     SEDSTRING="s@$SEARCH@$REPLACE@"
-    echo "$SEDSTRING"
     sed -i -e "$SEDSTRING" Gemfile
 fi
