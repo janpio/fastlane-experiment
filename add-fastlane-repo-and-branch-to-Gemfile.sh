bc8d5436a1e2c31077bb81de933df5074e816b78
Jan Piotrowski <piotrowski+git@gmail.com>
2018-12-18 13:46:00 +0100

    more debug

--- add-fastlane-repo-and-branch-to-Gemfile.sh
+++ add-fastlane-repo-and-branch-to-Gemfile.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "$REPO"
+echo "$BRANCH"
 if [ -n "$REPO" ] && [ -n "$BRANCH" ]; then
     SEARCH='gem "fastlane".*'
-    echo $SEARCH
+    echo "$SEARCH"
     REPLACE="gem \"fastlane\", :git => \"$REPO\", :branch => \"$BRANCH\""
-    echo $REPLACE
+    echo "$REPLACE"
     sed -i "s@$SEARCH@$REPLACE@" Gemfile
 fi
